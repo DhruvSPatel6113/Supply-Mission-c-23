7fd444c43497274e9c7ebdeadabb8edacec7703c
DhruvSPatel6113 <71432325+DhruvSPatel6113@users.noreply.github.com>
2020-10-13 06:41:14 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -74,15 +74,20 @@ function draw() {
 
 function keyPressed() {
  	if (keyCode === LEFT_ARROW) {
-		helicopterSprite.x=helicopterSprite.x-20; translation={x:-20,y:0} Matter.Body.translate(packageBody, translation) 
+		helicopterSprite.x=helicopterSprite.x-20;
+		translation={x:-20,y:0} 
+		Matter.Body.translate(packageBody, translation);
 	}
 	
 	else 
 	if (keyCode === RIGHT_ARROW) { 
-		helicopterSprite.x=helicopterSprite.x+20; translation={x:20,y:0} Matter.Body.translate(packageBody, translation) 
+		helicopterSprite.x=helicopterSprite.x+20;
+		translation={x:20,y:0} 
+		Matter.Body.translate(packageBody, translation);
 	} 
 	else 
-	if (keyCode === DOWN_ARROW) { Matter.Body.setStatic(packageBody,false); 
+	if (keyCode === DOWN_ARROW) { 
+		Matter.Body.setStatic(packageBody,false); 
 	}	
           
   }
